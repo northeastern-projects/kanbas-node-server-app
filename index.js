31945693da461fba15040409395c10c4342170b2
RahulR100 <rahulrao@email.com>
2023-12-03 20:17:43 +0000

    a6

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-// const express = require("express");
 import express from 'express';
 import HelloRoutes from './hello.js';
 import Lab5 from './lab5.js';
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
+import UserRoutes from './users/routes.js';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import 'dotenv/config';
 
 const app = express();
 
 app.use(
 	cors({
+		credentials: true,
 		origin: '*'
 	})
 );
+const sessionOptions = {
+	secret: 'any string',
+	resave: false,
+	saveUninitialized: false
+};
+app.use(session(sessionOptions));
+
 app.use(express.json());
 
 Lab5(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 HelloRoutes(app);
+UserRoutes(app);
 
-app.listen(process.env.PORT || 4000).on('listening', () => console.log('Server is running'));
+mongoose.connect(process.env.MONGO_URL).then(() => app.listen(process.env.PORT || 4000).on('listening', () => console.log('Server is running')));
